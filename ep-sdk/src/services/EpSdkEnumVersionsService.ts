26b37a394623e063853371dc921cd8103a02c29b
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2022-07-30 08:56:51 +0200

    enum versions service

---
+++ ep-sdk/src/services/EpSdkEnumVersionsService.ts
@@ -0,0 +1,151 @@
+import { EpSdkApiContentError } from "../EpSdkErrors";
+import EpSdkSemVerUtils from "../EpSdkSemVerUtils";
+import { 
+  Enum,
+  EnumsService, 
+  EnumVersion, 
+  EnumVersionsResponse 
+} from "../sep-openapi-node";
+import EpSdkEnumsService from "./EpSdkEnumsService";
+
+
+class EpSdkEnumVersionsService {
+
+  private getLatestVersionFromList = ({ enumVersionList }:{
+    enumVersionList: Array<EnumVersion>;
+  }): EnumVersion | undefined => {
+    const funcName = 'getLatestVersionFromList';
+    const logName = `${EpSdkEnumVersionsService.name}.${funcName}()`;
+
+    let latestEnumVersion: EnumVersion | undefined = undefined;
+    let latestVersionString: string = '0.0.0';
+    for(const enumVersion of enumVersionList) {
+      if(enumVersion.version === undefined) throw new EpSdkApiContentError(logName, this.constructor.name,'enumVersion.version === undefined', {    
+        enumVersion: enumVersion
+      });
+      const newVersionString: string = enumVersion.version;
+      if(EpSdkSemVerUtils.is_NewVersion_GreaterThan_OldVersion({
+        newVersionString: newVersionString,
+        oldVersionString: latestVersionString,
+      })) {
+        latestVersionString = newVersionString;
+        latestEnumVersion = enumVersion;
+      }
+    }
+    return latestEnumVersion;
+  }
+
+  public getVersionByVersion = async({ enumId, enumVersionString }:{
+    enumId: string;
+    enumVersionString: string;
+  }): Promise<EnumVersion | undefined> => {
+    const funcName = 'getVersionByVersion';
+    const logName = `${EpSdkEnumVersionsService.name}.${funcName}()`;
+
+    const enumVersionsResponse: EnumVersionsResponse = await EnumsService.getEnumVersionsForEnum({
+      enumId: enumId,
+      versions: [enumVersionString]
+    });
+    if(enumVersionsResponse.data === undefined || enumVersionsResponse.data.length === 0) return undefined;
+    if(enumVersionsResponse.data.length > 1) throw new EpSdkApiContentError(logName, this.constructor.name, 'enumVersionsResponse.data.length > 1', {
+      enumVersionsResponse: enumVersionsResponse
+    });
+    return enumVersionsResponse.data[0];
+  }
+
+  public getVersionsForEnumId = async({ enumId }:{
+    enumId: string;
+  }): Promise<Array<EnumVersion>> => {
+    const funcName = 'getVersionsForEnumId';
+    const logName = `${EpSdkEnumVersionsService.name}.${funcName}()`;
+
+    // wrong call
+    // const enumVersionsResponse: EnumVersionsResponse = await EnumsService.getEnumVersions({
+    //   ids: [enumId]
+    // });
+    const enumVersionsResponse: EnumVersionsResponse = await EnumsService.getEnumVersionsForEnum({
+      enumId: enumId,      
+    });
+    // CliLogger.trace(CliLogger.createLogEntry(logName, { code: ECliStatusCodes.SERVICE, details: {
+    //   enumVersionsResponse: enumVersionsResponse
+    // }}));
+    if(enumVersionsResponse.data === undefined || enumVersionsResponse.data.length === 0) return [];
+    return enumVersionsResponse.data;
+  }
+
+  public getVersionsForEnumName = async({ enumName, applicationDomainId }:{
+    applicationDomainId: string;
+    enumName: string;
+  }): Promise<Array<EnumVersion>> => {
+    const funcName = 'getVersionsForEnumName';
+    const logName = `${EpSdkEnumVersionsService.name}.${funcName}()`;
+
+    const enumObject: Enum | undefined = await EpSdkEnumsService.getByName({
+      applicationDomainId: applicationDomainId,
+      enumName: enumName
+    });
+    if(enumObject === undefined) return [];
+    if(enumObject.id === undefined) throw new EpSdkApiContentError(logName, this.constructor.name, 'enumObject.id === undefined', {
+      enumObject: enumObject
+    });
+    const enumVersionList: Array<EnumVersion> = await this.getVersionsForEnumId({ enumId: enumObject.id });
+    return enumVersionList;
+  }
+
+  public getLastestVersionString = async({ enumId }:{
+    enumId: string;
+  }): Promise<string | undefined> => {
+    const funcName = 'getLastestVersionString';
+    const logName = `${EpSdkEnumVersionsService.name}.${funcName}()`;
+
+    const enumVersionList: Array<EnumVersion> = await this.getVersionsForEnumId({ enumId: enumId });
+    // CliLogger.trace(CliLogger.createLogEntry(logName, { code: ECliStatusCodes.SERVICE, details: {
+    //   enumVersionList: enumVersionList
+    // }}));
+    const latestEnumVersion: EnumVersion | undefined = this.getLatestVersionFromList({ enumVersionList: enumVersionList });
+    if(latestEnumVersion === undefined) return undefined;
+    if(latestEnumVersion.version === undefined) throw new EpSdkApiContentError(logName, this.constructor.name, 'latestEnumVersion.version === undefined', {
+      latestEnumVersion: latestEnumVersion
+    });
+    return latestEnumVersion.version;
+  }
+
+  public getLatestVersionForEnumId = async({ enumId, applicationDomainId }: {
+    applicationDomainId: string;
+    enumId: string;
+  }): Promise<EnumVersion | undefined> => {
+    const funcName = 'getLatestVersionForEnumId';
+    const logName = `${EpSdkEnumVersionsService.name}.${funcName}()`;
+
+    applicationDomainId;
+    const enumVersionList: Array<EnumVersion> = await this.getVersionsForEnumId({ 
+      enumId: enumId,
+    });
+
+    const latestEnumVersion: EnumVersion | undefined = this.getLatestVersionFromList({ enumVersionList: enumVersionList });
+    return latestEnumVersion;
+  }
+
+  public getLastestVersionForEnumName = async({ applicationDomainId, enumName }:{
+    applicationDomainId: string;
+    enumName: string;
+  }): Promise<EnumVersion | undefined> => {
+    const funcName = 'getLastestVersionForEnumName';
+    const logName = `${EpSdkEnumVersionsService.name}.${funcName}()`;
+
+    const enumVersionList: Array<EnumVersion> = await this.getVersionsForEnumName({ 
+      enumName: enumName, 
+      applicationDomainId: applicationDomainId 
+    });
+    // CliLogger.trace(CliLogger.createLogEntry(logName, { code: ECliStatusCodes.SERVICE, details: {
+    //   enumVersionList: enumVersionList
+    // }}));
+
+    const latestEnumVersion: EnumVersion | undefined = this.getLatestVersionFromList({ enumVersionList: enumVersionList });
+    return latestEnumVersion;
+
+  }
+}
+
+export default new EpSdkEnumVersionsService();
+
---
+++ ep-sdk/test/specs/services/enumVersionsService.spec.ts
@@ -0,0 +1,211 @@
+import 'mocha';
+import { expect } from 'chai';
+import path from 'path';
+import { TestLogger } from '../../lib/TestLogger';
+import { TestContext } from '../../lib/TestContext';
+import TestConfig from '../../lib/TestConfig';
+import { TestUtils } from '../../lib/TestUtils';
+import { ApiError, ApplicationDomainResponse, ApplicationDomainsService, EnumResponse, EnumsService, EnumValue, EnumVersion, EnumVersionResponse } from '../../../src/sep-openapi-node';
+import { EpSdkError, EpSdkServiceError } from '../../../src/EpSdkErrors';
+import EpSdkEnumVersionsService from '../../../src/services/EpSdkEnumVersionsService';
+import EpSdkApplicationDomainsService from '../../../src/services/EpSdkApplicationDomainsService';
+
+const scriptName: string = path.basename(__filename);
+TestLogger.logMessage(scriptName, ">>> starting ...");
+
+const TestSpecId: string = TestUtils.getUUID();
+const ApplicationDomainName = `${TestConfig.getAppId()}/services/${TestSpecId}`;
+let ApplicationDomainId: string | undefined;
+const EnumName = `${TestConfig.getAppId()}-services-${TestSpecId}`;
+let EnumId: string | undefined;
+const EnumVersionString = '1.0.0';
+let EnumVersionId: string | undefined;
+const EnumValues: Array<EnumValue> = [
+  { label: 'one', value: 'one' },
+  { label: 'two', value: 'two' }
+];
+const EnumNextVersionString = '1.0.1';
+let EnumNextVersionId: string | undefined;
+
+
+describe(`${scriptName}`, () => {
+
+    beforeEach(() => {
+      TestContext.newItId();
+    });
+
+    after(async() => {
+      // delete application domain
+      await EpSdkApplicationDomainsService.deleteById({ applicationDomainId: ApplicationDomainId });
+    });
+
+    it(`${scriptName}: should create application domain`, async () => {
+      try {
+        const applicationDomainResponse: ApplicationDomainResponse = await ApplicationDomainsService.createApplicationDomain({
+          requestBody: {
+            name: ApplicationDomainName,
+          }
+        });
+        ApplicationDomainId = applicationDomainResponse.data.id;
+      } catch(e) {
+        if(e instanceof ApiError) expect(false, TestLogger.createApiTestFailMessage('failed')).to.be.true;
+        expect(e instanceof EpSdkError, TestLogger.createNotEpSdkErrorMesssage(e)).to.be.true;
+        expect(false, TestLogger.createEpSdkTestFailMessage('failed', e)).to.be.true;
+      }
+    });
+
+    it(`${scriptName}: should create enum`, async () => {
+      try {
+        const enumResponse: EnumResponse = await EnumsService.createEnum({
+          requestBody: {
+            applicationDomainId: ApplicationDomainId,
+            name: EnumName,
+            shared: false
+          }
+        });
+        EnumId = enumResponse.data.id;
+      } catch(e) {
+        if(e instanceof ApiError) expect(false, TestLogger.createApiTestFailMessage('failed')).to.be.true;
+        expect(e instanceof EpSdkError, TestLogger.createNotEpSdkErrorMesssage(e)).to.be.true;
+        expect(false, TestLogger.createEpSdkTestFailMessage('failed', e)).to.be.true;
+      }
+    });
+
+    it(`${scriptName}: should create enum version`, async () => {
+      try {
+        const enumVersionResponse: EnumVersionResponse = await EnumsService.createEnumVersionForEnum({
+          enumId: EnumId,
+          requestBody: {
+            description: `enum version for enum = ${EnumName}, id=${EnumId}`,
+            values: EnumValues,
+            version: EnumVersionString,
+          }
+        });
+        EnumVersionId = enumVersionResponse.data.id;
+      } catch(e) {
+        if(e instanceof ApiError) expect(false, TestLogger.createApiTestFailMessage('failed')).to.be.true;
+        expect(e instanceof EpSdkError, TestLogger.createNotEpSdkErrorMesssage(e)).to.be.true;
+        expect(false, TestLogger.createEpSdkTestFailMessage('failed', e)).to.be.true;
+      }
+    });
+
+    it(`${scriptName}: should get enum version by version`, async () => {
+      try {
+        const enumVersion: EnumVersion = await EpSdkEnumVersionsService.getVersionByVersion({ 
+          enumId: EnumId,
+          enumVersionString: EnumVersionString,
+        });
+        expect(enumVersion.version, TestLogger.createApiTestFailMessage('version mismatch')).to.eq(EnumVersionString);
+      } catch(e) {
+        if(e instanceof ApiError) expect(false, TestLogger.createApiTestFailMessage('failed')).to.be.true;
+        expect(e instanceof EpSdkError, TestLogger.createNotEpSdkErrorMesssage(e)).to.be.true;
+        expect(false, TestLogger.createEpSdkTestFailMessage('failed', e)).to.be.true;
+      }
+    });
+
+    it(`${scriptName}: should get enum versions for enum id`, async () => {
+      try {
+        const enumVersionList: Array<EnumVersion> = await EpSdkEnumVersionsService.getVersionsForEnumId({ enumId: EnumId });
+        expect(enumVersionList.length, TestLogger.createApiTestFailMessage('length not === 1')).to.eq(1);
+        const enumVersion: EnumVersion = enumVersionList[0];
+        expect(enumVersion.id, TestLogger.createApiTestFailMessage('id mismatch')).to.eq(EnumVersionId);
+      } catch(e) {
+        if(e instanceof ApiError) expect(false, TestLogger.createApiTestFailMessage('failed')).to.be.true;
+        expect(e instanceof EpSdkError, TestLogger.createNotEpSdkErrorMesssage(e)).to.be.true;
+        expect(false, TestLogger.createEpSdkTestFailMessage('failed', e)).to.be.true;
+      }
+    });
+
+    it(`${scriptName}: should get enum versions for enum name`, async () => {
+      try {
+        const enumVersionList: Array<EnumVersion> = await EpSdkEnumVersionsService.getVersionsForEnumName({ 
+          applicationDomainId: ApplicationDomainId,
+          enumName: EnumName 
+        });
+        expect(enumVersionList.length, TestLogger.createApiTestFailMessage('length not === 1')).to.eq(1);
+        const enumVersion: EnumVersion = enumVersionList[0];
+        expect(enumVersion.id, TestLogger.createApiTestFailMessage('id mismatch')).to.eq(EnumVersionId);
+      } catch(e) {
+        if(e instanceof ApiError) expect(false, TestLogger.createApiTestFailMessage('failed')).to.be.true;
+        expect(e instanceof EpSdkError, TestLogger.createNotEpSdkErrorMesssage(e)).to.be.true;
+        expect(false, TestLogger.createEpSdkTestFailMessage('failed', e)).to.be.true;
+      }
+    });
+
+    it(`${scriptName}: should create new enum version`, async () => {
+      try {
+        const enumVersionResponse: EnumVersionResponse = await EnumsService.createEnumVersionForEnum({
+          enumId: EnumId,
+          requestBody: {
+            description: `enum version for enum = ${EnumName}, id=${EnumId}`,
+            values: EnumValues,
+            version: EnumNextVersionString,
+          }
+        });
+        EnumNextVersionId = enumVersionResponse.data.id;
+      } catch(e) {
+        if(e instanceof ApiError) expect(false, TestLogger.createApiTestFailMessage('failed')).to.be.true;
+        expect(e instanceof EpSdkError, TestLogger.createNotEpSdkErrorMesssage(e)).to.be.true;
+        expect(false, TestLogger.createEpSdkTestFailMessage('failed', e)).to.be.true;
+      }
+    });
+
+
+    it(`${scriptName}: should get latest version string`, async () => {
+      try {
+        const latestVersionString: string = await EpSdkEnumVersionsService.getLastestVersionString({ enumId: EnumId });
+        expect(latestVersionString, TestLogger.createApiTestFailMessage('version string mismatch')).to.eq(EnumNextVersionString);
+      } catch(e) {
+        if(e instanceof ApiError) expect(false, TestLogger.createApiTestFailMessage('failed')).to.be.true;
+        expect(e instanceof EpSdkError, TestLogger.createNotEpSdkErrorMesssage(e)).to.be.true;
+        expect(false, TestLogger.createEpSdkTestFailMessage('failed', e)).to.be.true;
+      }
+    });
+
+    it(`${scriptName}: should get latest version for enum id`, async () => {
+      try {
+        const enumVersion: EnumVersion = await EpSdkEnumVersionsService.getLatestVersionForEnumId({ 
+          applicationDomainId: ApplicationDomainId,
+          enumId: EnumId 
+        });
+        expect(enumVersion.version, TestLogger.createApiTestFailMessage('version string mismatch')).to.eq(EnumNextVersionString);
+      } catch(e) {
+        if(e instanceof ApiError) expect(false, TestLogger.createApiTestFailMessage('failed')).to.be.true;
+        expect(e instanceof EpSdkError, TestLogger.createNotEpSdkErrorMesssage(e)).to.be.true;
+        expect(false, TestLogger.createEpSdkTestFailMessage('failed', e)).to.be.true;
+      }
+    });
+
+    it(`${scriptName}: should get latest version for enum name`, async () => {
+      try {
+        const enumVersion: EnumVersion | undefined = await EpSdkEnumVersionsService.getLastestVersionForEnumName({ 
+          applicationDomainId: ApplicationDomainId,
+          enumName: EnumName
+        });
+        expect(enumVersion, TestLogger.createApiTestFailMessage('enumVersion === undefined')).to.not.be.undefined;
+        expect(enumVersion.version, TestLogger.createApiTestFailMessage('version string mismatch')).to.eq(EnumNextVersionString);
+      } catch(e) {
+        if(e instanceof ApiError) expect(false, TestLogger.createApiTestFailMessage('failed')).to.be.true;
+        expect(e instanceof EpSdkError, TestLogger.createNotEpSdkErrorMesssage(e)).to.be.true;
+        expect(false, TestLogger.createEpSdkTestFailMessage('failed', e)).to.be.true;
+      }
+    });
+
+    it(`${scriptName}: should get latest version for enum name that doesn't exist`, async () => {
+      const NonExistentName = 'non-existent';
+      try {
+        const enumVersion: EnumVersion | undefined = await EpSdkEnumVersionsService.getLastestVersionForEnumName({ 
+          applicationDomainId: ApplicationDomainId,
+          enumName: NonExistentName
+        });
+        expect(enumVersion, TestLogger.createApiTestFailMessage('enumVersion === undefined')).to.be.undefined;
+      } catch(e) {
+        if(e instanceof ApiError) expect(false, TestLogger.createApiTestFailMessage('failed')).to.be.true;
+        expect(e instanceof EpSdkServiceError, TestLogger.createNotEpSdkErrorMesssage(e)).to.be.true;
+        const epSdkServiceError: EpSdkServiceError = e;
+        expect(epSdkServiceError.toString(), TestLogger.createApiTestFailMessage(`error does not contain ${NonExistentName}`)).to.contain(NonExistentName);
+      }
+    });
+
+});
+
