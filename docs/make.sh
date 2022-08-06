1996114a4a074330579736823d1694498a1bd879
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2022-08-06 13:22:33 +0200

    runs npm install if no typedoc found

--- docs/make.sh
+++ docs/make.sh
@@ -7,7 +7,13 @@ scriptName=$(basename $(test -L "$0" && readlink "$0" || echo "$0"));
 cd $scriptDir/../ep-sdk
   code=$?; if [[ $code != 0 ]]; then echo ">>> XT_ERROR - $code make html"; exit 1; fi
 npm run build:tsdocs
-  code=$?; if [[ $code != 0 ]]; then echo ">>> XT_ERROR - $code make html"; exit 1; fi
+  code=$?;
+  # run npm install and try again
+  if [[ $code != 0 ]]; then
+    npm install;
+    npm run build:tsdocs;
+    code=$?; if [[ $code != 0 ]]; then echo ">>> XT_ERROR - $code make html"; exit 1; fi    
+  fi
 
 # generate rst docs
 cd $scriptDir
