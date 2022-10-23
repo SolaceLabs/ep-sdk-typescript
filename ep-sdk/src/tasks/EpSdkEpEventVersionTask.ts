8ca9d02352278992977f415121b686b70e09903a
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2022-10-23 11:53:27 +0200

    fixed doc build

--- ep-sdk/src/tasks/EpSdkEpEventVersionTask.ts
+++ ep-sdk/src/tasks/EpSdkEpEventVersionTask.ts
@@ -24,7 +24,7 @@ import EpSdkEnumVersionService from '../services/EpSdkEnumVersionsService';
 import EpSdkEpEventVersionsService from '../services/EpSdkEpEventVersionsService';
 
 /** @category EpSdkEpEventVersionTask */
-type TEpSdkEpEventVersionTask_Settings_DeliveryDescriptor = Pick<DeliveryDescriptor, "brokerType" >;
+export type TEpSdkEpEventVersionTask_Settings_DeliveryDescriptor = Pick<DeliveryDescriptor, "brokerType" >;
 export type TEpSdkEpEventVersionTask_Settings = Required<Pick<EventVersion, "description" | "displayName" | "stateId" | "schemaVersionId" >> & TEpSdkEpEventVersionTask_Settings_DeliveryDescriptor;
 type TEpSdkEpEventVersionTask_CompareObject = Partial<TEpSdkEpEventVersionTask_Settings> & Pick<EventVersion, "deliveryDescriptor"> & Partial<Pick<EventVersion, "version">>;
 
